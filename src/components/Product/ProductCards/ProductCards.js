1cd5850b99f4f19f653ce6ef677ab4a8be822bbd
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-08-07 18:18:34 -0700

    attempt ingredients animation

--- src/components/Product/ProductCards/ProductCards.js
+++ src/components/Product/ProductCards/ProductCards.js
@@ -3,25 +3,33 @@ import "./ProductCards.css";
 import productcards from './productcards.json';
 
 class ProductCards extends Component {
+    
+
     render() {
 
         return (
+            
             <div className="card-columns">
 
                 {productcards.map(productcards => (
-                    <div className="card border-dark">
+                    <div className="card border-dark" key={productcards.id}>
                         <img className="card-img-top" src={productcards.image} alt={productcards.imageName} />
                         <div className="card-body card-info">
-                        <hr />
-                            <h4 className="card-title text-center">{productcards.name}</h4>
-                            <p className="card-text text-left"><b>Natural Ingrdients:</b> {productcards.ingredients}</p>
+                            <hr />
+                            
+                            {/* <img id="sliderTrigger" data-box={productcards.id} className="productSlider" src={require("../../images/arrowdown.png")} alt="downarrowthing" /> */}
+                            <div className="slideUp" id={productcards.id}>
+                           
+                                <h4 className="card-title text-center">{productcards.name}</h4>
+                                <p className="card-text text-left"><b>Natural Ingrdients:</b> {productcards.ingredients}</p>
+                            </div>
                         </div>
                     </div>
                 ))}
 
             </div>
 
-        )
+        );
     };
 };
 
