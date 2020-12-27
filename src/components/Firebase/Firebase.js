70c3267698bdd137e3e113acf252df4590a57008
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2020-12-26 21:35:43 -0800

    firebase update scripts and appid

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -7,7 +7,8 @@ var config = {
     databaseURL: "https://auntiehotmessdb.firebaseio.com",
     projectId: "auntiehotmessdb",
     storageBucket: "auntiehotmessdb.appspot.com",
-    messagingSenderId: "1087867434162"
+    messagingSenderId: "1087867434162",
+    appId: "1:1087867434162:web:750971f59f966907"
 };
 firebase.initializeApp(config);
 
